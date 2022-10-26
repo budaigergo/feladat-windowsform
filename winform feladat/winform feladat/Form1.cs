6aecff47f544b05d65f3eef7d027bcd464accc0a
budaigergo <112852798+budaigergo@users.noreply.github.com>
2022-10-26 12:00:52 +0200

    Add files via upload

---
+++ winform feladat/winform feladat/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_feladat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(numericUpDown1.Text))
+            {
+                MessageBox.Show("Nem adta meg az összes adatot!");
+            }
+            if (String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Nem adta meg az összes adatot!");
+            }
+            if (String.IsNullOrEmpty(comboBox1.Text))
+            {
+                MessageBox.Show("Nem adta meg az összes adatot!");
+            }
+            if (String.IsNullOrEmpty(dateTimePicker1.Text))
+            {
+                MessageBox.Show("Nem adta meg az összes adatot!");
+            }
+            if (String.IsNullOrEmpty(comboBox2.Text))
+            {
+                MessageBox.Show("Nem adta meg az összes adatot!");
+            }
+
+
+        }
+    }
+}
